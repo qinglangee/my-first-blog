32f4572536a1c6b3ab304bc80b5c189786c0b189
zhch <zhch@163.com>
2020-08-18 07:11:43 +0800

    Basic view now returns minimal HTML

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from lists import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    # path('', include('blog.urls'))
+    # path('', include('lists.urls'))
+    # path(r'^$', views.home_page, name='home'),
+    path(r'', views.home_page, name='home'),
 ]
