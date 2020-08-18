85b85ffbffc31cb8d18de6f2e8a9a94d00d05d5b
zhch <zhch@163.com>
2020-08-18 16:19:32 +0800

    add some new tests

--- lists/tests.py
+++ lists/tests.py
@@ -8,10 +8,12 @@ from lists.views import home_page
 # Create your tests here.
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
     
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_Post_request(self):
+        response = self.client.post("/", data={'item_text':'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
